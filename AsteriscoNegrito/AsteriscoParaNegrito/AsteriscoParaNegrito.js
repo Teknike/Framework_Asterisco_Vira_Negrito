7252ae6335fbeb43b34cf0baedbf2a41fb665a3d
tiagocunico <tiagobcunico@gmail.com>
2022-04-14 15:35:19 -0300

    Funciona o basico

---
+++ AsteriscoNegrito/AsteriscoParaNegrito/AsteriscoParaNegrito.js
@@ -0,0 +1,99 @@
+﻿// Keep these lines for a best effort IntelliSense of Visual Studio 2017 and higher.
+/// <reference path="./../../Packages/Beckhoff.TwinCAT.HMI.Framework.12.754.4/runtimes/native1.12-tchmi/TcHmi.d.ts" />
+
+/*
+ * Generated 4/14/2022 8:18:21 AM
+ * Copyright (C) 2022
+ */
+var TcHmi;
+(function (/** @type {globalThis.TcHmi} */ TcHmi) {
+    let Controls;
+    (function (/** @type {globalThis.TcHmi.Controls} */ Controls) {
+        let AsteriscoNegrito;
+        (function (AsteriscoNegrito) {
+            class AsteriscoParaNegrito extends TcHmi.Controls.System.TcHmiControl {
+                /*
+                Attribute philosophy
+                --------------------
+                - Local variables are not set while definition in class, so they have the value 'undefined'.
+                - On compile the Framework sets the value from HTML or from theme (possibly 'null') via normal setters.
+                - The "changed detection" in the setter will result in processing the value only once while compile.
+                - Attention: If we have a Server Binding on an Attribute the setter will be called once with null to initialize and later with the correct value.
+                */
+                /**
+                 * Constructor of the control
+                 * @param {JQuery} element Element from HTML (internal, do not use)
+                 * @param {JQuery} pcElement precompiled Element (internal, do not use)
+                 * @param {TcHmi.Controls.ControlAttributeList} attrs Attributes defined in HTML in a special format (internal, do not use)
+                 * @returns {void}
+                 */
+                constructor(element, pcElement, attrs) {
+                    /** Call base class constructor */
+                    super(element, pcElement, attrs);
+                }
+                /**
+                  * If raised, the control object exists in control cache and constructor of each inheritation level was called.
+                  * Call attribute processor functions here to initialize default values!
+                  */
+                __previnit() {
+                    // Fetch template root element
+                    this.__elementTemplateRoot = this.__element.find('.TcHmi_Controls_AsteriscoNegrito_AsteriscoParaNegrito-Template');
+                    if (this.__elementTemplateRoot.length === 0) {
+                        throw new Error('Invalid Template.html');
+                    }
+                    // Call __previnit of base class
+                    super.__previnit();
+                }
+                /**
+                 * Is called during control initialize phase after attribute setter have been called based on it's default or initial html dom values.
+                 * @returns {void}
+                 */
+                __init() {
+                    super.__init();
+                }
+                /**
+                * Is called by tachcontrol() after the control instance gets part of the current DOM.
+                * Is only allowed to be called from the framework itself!
+                */
+                __attach() {
+                    super.__attach();
+                    /**
+                     * Initialize everything which is only available while the control is part of the active dom.
+                     */
+                }
+                /**
+                * Is called by tachcontrol() after the control instance is no longer part of the current DOM.
+                * Is only allowed to be called from the framework itself!
+                */
+                __detach() {
+                    super.__detach();
+                    /**
+                     * Disable everything which is not needed while the control is not part of the active dom.
+                     * No need to listen to events for example!
+                     */
+                }
+                /**
+                * Destroy the current control instance.
+                * Will be called automatically if system destroys control!
+                */
+                destroy() {
+                    /**
+                    * While __keepAlive is set to true control must not be destroyed.
+                    */
+                    if (this.__keepAlive) {
+                        return;
+                    }
+                    super.destroy();
+                    /**
+                    * Free resources like child controls etc.
+                    */
+                }
+            }
+            AsteriscoNegrito.AsteriscoParaNegrito = AsteriscoParaNegrito;
+        })(AsteriscoNegrito = Controls.AsteriscoNegrito || (Controls.AsteriscoNegrito = {}));
+    })(Controls = TcHmi.Controls || (TcHmi.Controls = {}));
+})(TcHmi || (TcHmi = {}));
+/**
+* Register Control
+*/
+TcHmi.Controls.registerEx('AsteriscoParaNegrito', 'TcHmi.Controls.AsteriscoNegrito', TcHmi.Controls.AsteriscoNegrito.AsteriscoParaNegrito);
